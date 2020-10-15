e78fc614b9471601d75b13bee0ce20685a42fcb7
박승권 <yevgnenll@gmail.com>
2020-10-16 03:59:56 +0900

    테스트 케이스 추가
    동시성 체크 로직 추가함

--- src/test/kotlin/me/yevgnenll/jpa/RegisterMember.kt
+++ src/test/kotlin/me/yevgnenll/jpa/RegisterMember.kt
@@ -2,8 +2,8 @@ package me.yevgnenll.jpa
 
 import me.yevgnenll.jpa.entity.Member
 import org.junit.Test
-import javax.persistence.EntityManager
 import org.junit.Assert.*
+import javax.persistence.EntityManager
 
 class RegisterMember {
 
@@ -12,14 +12,21 @@ class RegisterMember {
     JpaSampleApplication.jpa { em: EntityManager ->
       run {
         var member = Member("member1", "josh")
+        var member2 = Member("member2", "bodguy")
         em.persist(member)
+        em.persist(member2)
       }
     }
 
     JpaSampleApplication.jpa { em: EntityManager ->
       run {
-        var member:Member = em.find(Member::class.java, "member1")
+        var member = em.find(Member::class.java, "member1")
+        var member2 = em.find(Member::class.java, "member2")
+        var member1 = em.find(Member::class.java, "member1")
         assertEquals(member.id, "member1")
+        assertNotEquals(member1, member2)
+        assertEquals(member, member1)
+        assertTrue(member == member1)
       }
     }
   }
