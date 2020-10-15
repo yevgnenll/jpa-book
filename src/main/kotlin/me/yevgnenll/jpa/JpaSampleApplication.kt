91ed050e93931445721edcd9c411e300242455da
박승권 <yevgnenll@gmail.com>
2020-10-16 03:42:20 +0900

    entityFactory를 함께 사용하도록 수정
    람다를 이용해 transaction 범위로 들어가야 하는 함수를 넘기도록 설계함

--- src/main/kotlin/me/yevgnenll/jpa/JpaSampleApplication.kt
+++ src/main/kotlin/me/yevgnenll/jpa/JpaSampleApplication.kt
@@ -5,23 +5,24 @@ import javax.persistence.EntityManagerFactory
 import javax.persistence.EntityTransaction
 import javax.persistence.Persistence
 
-// 이거 AOP 로 빼면 좋을거 같은데?
-fun main(args:Array<String>) {
-  var emf: EntityManagerFactory = Persistence.createEntityManagerFactory("jpabook")
-  var em:EntityManager = emf.createEntityManager();
-  var tx:EntityTransaction = em.transaction;
+class JpaSampleApplication {
+  companion object {
+    fun jpa(callback:(EntityManager) -> Unit) {
+      var emf: EntityManagerFactory = Persistence.createEntityManagerFactory("jpabook")
+      var em:EntityManager = emf.createEntityManager();
+      var tx:EntityTransaction = em.transaction;
 
-  try {
-    tx.begin()
-    logic(em)
-    tx.commit()
-  } catch (ex:Exception) {
-    tx.rollback()
-  } finally {
-    em.close()
+      try {
+        tx.begin()
+        callback(em)
+        tx.commit()
+      } catch (ex:Exception) {
+        tx.rollback()
+      } finally {
+        em.close()
+      }
+      emf.close()
+    }
   }
-  emf.close()
 }
-fun logic(em:EntityManager) {
 
-}
