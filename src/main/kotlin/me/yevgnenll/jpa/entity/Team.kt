067160a2748723768f427ab7ec597b0e552cd106
박승권 <yevgnenll@gmail.com>
2020-10-23 00:14:52 +0900

    양방향 mapping

--- src/main/kotlin/me/yevgnenll/jpa/entity/Team.kt
+++ src/main/kotlin/me/yevgnenll/jpa/entity/Team.kt
@@ -3,6 +3,7 @@ package me.yevgnenll.jpa.entity
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.OneToMany
 
 @Entity
 data class Team(
@@ -11,4 +12,7 @@ data class Team(
   var id:String,
   @Column(name = "name")
   var name:String,
-)
+) {
+  @OneToMany(mappedBy = "team")
+  var members: List<Member>? = ArrayList()
+}
---
+++ src/main/kotlin/me/yevgnenll/jpa/example/BiDirection.kt
@@ -0,0 +1,20 @@
+package me.yevgnenll.jpa.example
+
+import me.yevgnenll.jpa.JpaSampleApplication
+import me.yevgnenll.jpa.entity.Member
+import me.yevgnenll.jpa.entity.Team
+import javax.persistence.EntityManager
+
+fun main() {
+  JpaSampleApplication.jpa { run { testSave(it) } }
+  JpaSampleApplication.jpa { run { biDirection(it) } }
+}
+
+fun biDirection(em: EntityManager) {
+  var team = em.find(Team::class.java, "team1")
+  var members:List<Member> = team.members!!
+
+  for (member in members) {
+    println(member)
+  }
+}
