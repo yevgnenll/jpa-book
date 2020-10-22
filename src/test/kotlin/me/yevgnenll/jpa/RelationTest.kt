a72cddd4198974b2f416742725801c94b487da8e
박승권 <yevgnenll@gmail.com>
2020-10-23 04:00:39 +0900

    양방향 관계에서 순수객체까지 고려할것

---
+++ src/test/kotlin/me/yevgnenll/jpa/RelationTest.kt
@@ -0,0 +1,58 @@
+package me.yevgnenll.jpa
+
+import me.yevgnenll.jpa.entity.Member
+import me.yevgnenll.jpa.entity.Team
+import org.junit.Test
+
+class RelationTest {
+
+  @Test
+  fun `순수한객체 양방향`() {
+    var team = Team("team1", "팀1")
+    var member1 = Member("member1", "회원1")
+    var member2 = Member("member2", "회원2")
+
+    member1.team = team
+    member2.team = team
+
+    var members = team.members
+    println("size: ${members.size}")
+  }
+
+  @Test
+  fun `순수한객체 양방향 2`() {
+    var team = Team("team1", "팀1")
+
+    var member1 = Member("member1", "회원1")
+    member1.team = team
+
+    var member2 = Member("member2", "회원2")
+    member2.team = team
+
+    team.members = mutableListOf(member1, member2)
+
+    var members = team.members
+    println("size: ${members.size}")
+  }
+
+  @Test
+  fun `orm 양방향`() {
+    JpaSampleApplication.jpa { run {
+      var team = Team("team1", "팀1")
+      it.persist(team)
+
+      var member1 = Member("member1", "회원1")
+      member1.team = team
+      it.persist(member1)
+
+      var member2 = Member("member2", "회원2")
+      member2.team = team
+      it.persist(member2)
+
+      team.members = mutableListOf(member1, member2)
+
+      var members = team.members
+    } }
+  }
+
+}
