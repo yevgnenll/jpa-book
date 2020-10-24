0e8cbe6299dd241bde24a248d4be323916309cda
박승권 <yevgnenll@gmail.com>
2020-10-24 10:59:23 +0900

    mapped super class

---
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/mappedsuper/BaseEntity.kt
@@ -0,0 +1,15 @@
+package me.yevgnenll.jpa.enhanced.mappedsuper
+
+import java.time.LocalDateTime
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+data class BaseEntity (
+  @Id
+  @GeneratedValue
+  var id: Long,
+  var name: String,
+  var createdAt: LocalDateTime
+)
---
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/mappedsuper/Member.kt
@@ -0,0 +1,15 @@
+package me.yevgnenll.jpa.enhanced.mappedsuper
+
+import java.time.LocalDateTime
+import javax.persistence.AttributeOverride
+import javax.persistence.Column
+import javax.persistence.Entity
+
+@Entity(name = "mapped_super_member")
+@AttributeOverride(name = "id", column = Column(name = "member_id"))
+class Member (
+  id: Long,
+  name: String,
+  createdAt: LocalDateTime,
+  var email: String
+): BaseEntity(id, name, createdAt)
