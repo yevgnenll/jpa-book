3baa8963ab2fa4643767b46cda8e720f16cdf3c8
박승권 <yevgnenll@gmail.com>
2020-10-24 11:30:17 +0900

    @EmbeddedId 적용

--- src/main/kotlin/me/yevgnenll/jpa/enhanced/compound/Parent.kt
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/compound/Parent.kt
@@ -1,18 +1,10 @@
 package me.yevgnenll.jpa.enhanced.compound
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.IdClass
+import javax.persistence.*
 
 @Entity
-@IdClass(ParentId::class)
 class Parent (
-  @Id
-  @Column(name = "parent_id1")
-  var id1:String,
-
-  @Id
-  @Column(name = "parent_id2")
-  var id2:String
+  @EmbeddedId
+  var id: ParentId,
+  var name: String
 )
--- src/main/kotlin/me/yevgnenll/jpa/enhanced/compound/ParentId.kt
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/compound/ParentId.kt
@@ -3,10 +3,15 @@ package me.yevgnenll.jpa.enhanced.compound
 import lombok.EqualsAndHashCode
 import lombok.NoArgsConstructor
 import java.io.Serializable
+import javax.persistence.Column
+import javax.persistence.Embeddable
 
 @EqualsAndHashCode
 @NoArgsConstructor
+@Embeddable
 class ParentId (
+  @Column
   var id1: String,
+  @Column
   var id2: String
 ): Serializable
