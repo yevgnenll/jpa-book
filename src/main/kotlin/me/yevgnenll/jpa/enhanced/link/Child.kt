38e3df9911de36e562dfdaeaeee0d99832a13c43
박승권 <yevgnenll@gmail.com>
2020-10-24 14:52:24 +0900

    M:N relation

---
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/link/Child.kt
@@ -0,0 +1,14 @@
+package me.yevgnenll.jpa.enhanced.link
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity(name = "link_child")
+class Child (
+  @Id @GeneratedValue
+  @Column(name = "child_id")
+  var id: Long,
+  var name: String
+)
---
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/link/Parent.kt
@@ -0,0 +1,20 @@
+package me.yevgnenll.jpa.enhanced.link
+
+import javax.persistence.*
+
+@Entity(name = "link_parent")
+class Parent (
+  @Id
+  @GeneratedValue
+  @Column(name = "parent_id")
+  var id: Long,
+  var name: String,
+) {
+  @ManyToMany
+  @JoinTable(
+    name = "parent_child",
+    joinColumns = [JoinColumn(name = "parent_id")],
+    inverseJoinColumns = [JoinColumn(name = "child_id")]
+  )
+  var child: MutableList<Child> = mutableListOf()
+}
