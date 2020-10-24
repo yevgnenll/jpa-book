66505f2744652412899f4e53578fa520d3ce5378
박승권 <yevgnenll@gmail.com>
2020-10-24 10:12:00 +0900

    single table

---
+++ src/main/kotlin/me/yevgnenll/jpa/enhanced/single/Item.kt
@@ -0,0 +1,24 @@
+package me.yevgnenll.jpa.enhanced.single
+
+import javax.persistence.*
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+class Item (
+  @Id
+  @GeneratedValue
+  @Column(name = "item_id")
+  var id:Long,
+  var name:String,
+  var price:Int
+)
+
+@Entity
+@DiscriminatorValue("A")
+class Album (
+  id: Long,
+  name: String,
+  price: Int,
+  var artist:String
+): Item(id, name, price)
