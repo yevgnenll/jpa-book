5335de9592886604c8f02ca4d782c5b2e166f5a6
박승권 <yevgnenll@gmail.com>
2020-10-30 00:30:22 +0900

    embedded 적용 전

--- src/main/kotlin/me/yevgnenll/jpa/vtype/Member.kt
+++ src/main/kotlin/me/yevgnenll/jpa/vtype/Member.kt
@@ -1,13 +1,19 @@
 package me.yevgnenll.jpa.vtype
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
+import java.util.*
+import javax.persistence.*
 
 @Entity
 class Member (
   @Id @GeneratedValue
   var id: Long,
   var name: String,
-  var age: Int
+  @Temporal(TemporalType.DATE)
+  var startDate: Date,
+  @Temporal(TemporalType.DATE)
+  var endDate: Date,
+
+  var city: String,
+  var street: String,
+  var zipCode: String
 )
